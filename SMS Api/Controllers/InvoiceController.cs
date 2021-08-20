468ba834218ed8f99f5942cc2aa9d88418733829
S HARSHA VARDHAN <925311@cognizant.com>
2021-08-21 00:31:40 +0530

    no-css

---
+++ SMS Api/Controllers/InvoiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SMS_Api.Models;
+
+namespace SMS_Api.Controllers
+{
+    public class InvoiceController : ApiController
+    {
+        private SMSEntities db = new SMSEntities();
+
+        public IEnumerable<Invoice> Get()
+
+        {
+
+            return db.Invoices.ToList();
+
+
+
+        }
+       
+        
+
+    }
+}
---
+++ SMS Api/Controllers/VendordetailsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SMS_Api.Models;
+
+namespace SMS_Api.Controllers
+{
+    public class VendordetailsController : ApiController
+    {
+        private SMSEntities db = new SMSEntities();
+        public IEnumerable<Vendor> Get()
+
+        {
+
+            return db.Vendors.ToList();
+
+
+
+        }
+    }
+}
